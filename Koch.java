8e670d8ea1981316f2e281904b47b4f1bab8e5b4
Daniel Herschel <danielh2772@gmail.com>
2025-12-28 15:40:56 +0200

    finished hw

--- Koch.java
+++ Koch.java
@@ -12,23 +12,43 @@ public class Koch {
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
-		curve(Integer.parseInt(args[0]),
-			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
-		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+         */
+
+//		curve(Integer.parseInt(args[0]),
+//			  Double.parseDouble(args[1]), Double.parseDouble(args[2]),
+//		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+
 
 		/*
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
+		 */
 		snowFlake(Integer.parseInt(args[0]));
-		*/
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+			return;
+		}
+		// compute the points at 1/3 and 2/3 along the segment
+		double xA = x1 + (x2 - x1) / 3.0;
+		double yA = y1 + (y2 - y1) / 3.0;
+
+		double xB = x1 + 2.0 * (x2 - x1) / 3.0;
+		double yB = y1 + 2.0 * (y2 - y1) / 3.0;
+
+		// find the point at the peak of the curve
+		double xPeak = (x1 + x2) / 2.0 - (Math.sqrt(3) * (y2 - y1)) / 6.0;
+		double yPeak = (y1 + y2) / 2.0 + (Math.sqrt(3) * (x2 - x1)) / 6.0;
+
+		// recursive subdivision calls
+		curve(n - 1, x1, y1, xA, yA);
+		curve(n - 1, xA, yA, xPeak, yPeak);
+		curve(n - 1, xPeak, yPeak, xB, yB);
+		curve(n - 1, xB, yB, x2, y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -36,7 +56,10 @@ public class Koch {
 		// A little tweak that makes the drawing look better
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
+
 		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		curve(n, 0.1, 0.8, 1.0, 0.8);
+		curve(n,1.0, 0.8, 0.55, 0.1);
+		curve(n,0.55, 0.1, 0.1, 0.8);
 	}
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -1,18 +1,38 @@
 /** Draws ths Sierpinski Triangle fractal. */
 public class Sierpinski {
 	
-	public static void main(String[] args) {
-		sierpinski(Integer.parseInt(args[0]));
-	}
+	public static void main(String[] args) { sierpinski(Integer.parseInt(args[0])); }
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		StdDraw.setCanvasSize(800, 800);
+		StdDraw.setScale(-1.0, 1.0);
+
+		sierpinski(n, 0.0, 1.0, -1.0, -1.0, 1.0, -1.0);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
-		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+								 double y1, double y2, double y3) {
+		// break condition
+		if (n == 0) {
+			StdDraw.line(x1, x2, x3, y1);
+			StdDraw.line(x3, y1, y2, y3);
+			StdDraw.line(y2, y3, x1, x2);
+			return;
+		}
+
+		// midpoints of each side
+		double x12 = (x1 + x3) / 2.0;
+		double y12 = (x2 + y1) / 2.0;
+		double x23 = (x3 + y2) / 2.0;
+		double y23 = (y1 + y3) / 2.0;
+		double x31 = (y2 + x1) / 2.0;
+		double y31 = (y3 + x2) / 2.0;
+
+		// recursion calls
+		sierpinski(n - 1, x1, x2, x12, y12, x31, y31);
+		sierpinski(n - 1, x12, y12, x3, y1, x23, y23);
+		sierpinski(n - 1, x31, y31, x23, y23, y2, y3);
 	}
 }
